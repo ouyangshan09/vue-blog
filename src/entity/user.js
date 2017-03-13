0b6de2b4a5763c9ccc27141499aa0a3fad72d496
ouyangshan09 <ouyangshan09@gmail.com>
2017-03-13 23:29:19 +0800

    1、创建user实体类，想使用建造摄设计模式应用，该类可能在多个地方被使用到

---
+++ src/entity/user.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ouyangshan09 on 2017/3/13.
+ * 用户实体类型描述，使用建造者设计模式
+ * @author Ouyang
+ * @version 1.0
+ */
+class User{
+    constructor(){
+        this.id = '';
+        this.account = '';
+        this.password = '';
+    }
+}
+
+export default User;
--- src/server/Login.vue
+++ src/server/Login.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container">
-        <el-form class="login-form" :model="user" ref="userForm">
+        <el-form class="login-form" :model="user" :rules="rules" ref="userForm" v-loading="loadObj.loading" :element-loading-text="loadObj.loadingText">
             <h3 class="title">欢迎登录后台管理系统</h3>
-            <el-form-item>
+            <el-form-item prop="account">
                 <el-input type="text" v-model="user.account" placeholder="帐号"></el-input>
             </el-form-item>
-            <el-form-item>
+            <el-form-item prop="password">
                 <el-input type="password" v-model="user.password" placeholder="密码"></el-input>
             </el-form-item>
             <el-form-item>
@@ -25,18 +25,51 @@
     export default {
         data(){
             return {
+                loadObj: {
+                    loading: false,
+                    loadingText: '页面跳转中',
+                    loadingflag: false,
+                },
+                loading: false,
                 user: {
                     account: '',
                     password: ''
-                }
+                },
+                rules: {
+                    account: [{
+                        required: true,
+                        message: '帐号不能为空',
+                        tigger: 'blur'
+                    }],
+                    password: [{
+                        required: true,
+                        message: '请输入密码',
+                        tigger: 'blur'
+                    }],
+                },
+                msgObj: {
+                    errorMessage: false,
+                },
             }
         },
         methods: {
             toRegister(){
-
+                // this.$router.push({ path: '/register'});
             },
             toManager(){
-
+                const userObj = {
+                    account: this.user.account,
+                    password: this.user.password
+                }
+                this.$refs['userForm'].validate(value => {
+                    console.log("12312");
+                    if(value){
+                        console.log('success: ', value);
+                    }else {
+                        console.log('error submit');
+                        return false;
+                    }
+                });
             }
         },
     }
