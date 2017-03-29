60a0c68962077557155172c3865a519cb2fa8cca
Ouyang <ouyangshan09@gmail.com>
2017-03-29 23:14:58 +0800

    1、完成login重构

--- src/client/Manager.vue
+++ src/client/Manager.vue
@@ -1,4 +1,10 @@
 <template>
-    <div>管理,需要在登录后</div>
+    <!--<div>管理,需要在登录后</div>-->
+    <div class="container">
+        <div class="oy-console--top">
+            <div class="content"></div>
+        </div>
+        <div class="oy-console--left"></div>
+    </div>
 </template>
 <script></script>
