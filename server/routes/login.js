2de688744b2a2209d09edef182d577bf7ff7c518
Ouyang <ouyangshan09@gmail.com>
2017-03-29 16:34:41 +0800

    1、无

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,13 +3,8 @@
  * 注册业务路由
  * @author Ouyang
  */
-// const express = require('express');
-// const router = express.Router();
-// const db = require('../db/dbConnection');
-// const User = require('../model/User');
 import express from 'express';
 import UserApi from '../api/user.api';
-import Login from '../constant/login';
 import * as Response from '../responseObj/loginResponseType';
 import * as Utils from '../utils/infoUtils';
 import * as Exception from '../exception/oyException';
@@ -56,46 +51,6 @@ router.post('/login', verifyUserObj, (request, response, next) => {
     }catch (e){
         next(new Exception.Logic());
     }
-    // let userModel = db.model(User.getName());
-    // userModel.find({
-    //     account: account,
-    //     password: password
-    // }).then(value => {
-    //     console.log('login value: ', value);
-    // });
-});
-
-router.post('/mongodTest', (request, response, next) => {
-    const {
-        account = null,
-        password = null
-    } = request.body.user;
-    let UserModel = db.model(User.getName());
-    // UserModel.find({
-    //     account: account
-    // }).then(value => {
-    //     console.log('find: ', value);
-    // });
-    let userEntity = new UserModel({account: account, password: password});
-    // UserModel.create({
-    //     account: account,
-    //     password: password
-    // }).then(value => {
-    //     console.log("value: ", value);
-    // });
-    // userEntity.find({
-    //     account: account
-    // }).then(value => {
-    //     console.log("value: ", value);
-    //     return value;
-    // }).then(value => {
-    //     console.log("value2: ", value);
-    // });
-    // userEntity.findTest();
-    console.log('User Module: ', userEntity);
-    console.log('login request: ', `account: ${account} - password: ${password}`);
-
-    response.send('login');
 });
 
 module.exports = router;
