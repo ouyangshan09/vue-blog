6e780def14e9d3df876dc14c62729fa2aa128368
Ouyangshan <ouyangshan09@gmail.com>
2017-04-14 17:53:09 +0800

    1、使用fixed 修复，SimpledMDE 带来的高度和宽度问题 2、需要添加SimpledMDE控件

--- src/client/List.vue
+++ src/client/List.vue
@@ -1,14 +1,14 @@
 <template>
-	<div class="clearfix article">
+	<div class="article">
 		<!--文章列表-->
-		<aside class="article-list">
+		<aside class="article-list article-pos">
 			<list @select="handleArticleList" style="padding-bottom: 60px">
 				<list-item index="-1" title="标题1" content="内容1" last-date="2017-04-13" author="欧阳"></list-item>
 				<list-item v-for="o in 10" :key="o" :index="o + ''"></list-item>
 			</list>
 		</aside>
 		<section class="article-content">
-			<div class="oy-article-content">
+			<div class="">
 				<!--编辑内容-->
 				<textarea id="editor" style=""></textarea>
 			</div>
